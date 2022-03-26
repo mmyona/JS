6de55c574ce476324fa0aa13027c54c402c4fb8f
mmyona <hyon2001@ajou.ac.kr>
2022-03-26 14:31:12 +0900

    2022.03.26

--- 웹개발 기본기/웹기초/fetch.js
+++ 웹개발 기본기/웹기초/fetch.js
@@ -1,5 +1,5 @@
 fetch('https://www.google.com')
-.then((response)=>response.text()) 
+.then((response)=>response.text()) //콜백함수
 .then((result)=>{console.log(result);}); //콜백함수*****
 // 콜백함수 : 콜백함수 이전의 함수가 실행된 후에야 실행된다.
 
@@ -56,3 +56,47 @@ fetch('https://learn.codeit.kr/api/members/2',{ //수정 대상
 })
 .then((response)=>response.text()) 
 .then((result)=>{console.log(result);});
+
+//content-type 추가하는 법
+
+/*const newMember = {
+    name: 'Henry',
+    email: 'henry@codeit.kr',
+    department: 'engineering',
+  };*/
+  
+  fetch('https://learn.codeit.kr/api/members', {
+    method: 'POST',
+    headers: { // 추가된 부분
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newMember),
+  })
+    .then((response) => response.text())
+    .then((result) => { console.log(result); });
+
+//fetch 함수와 비동기 실행
+
+/*promise 객체
+: fetch함수 -> promise 객체를 리턴
+: promise 객체가 fulfilled 됐을 때 -> promise 객체는 작업 성공 결과(=response, result) 를 갖게 된다.
+-> then 메소드를 사용하면...
+promise 객체가 fulfilled 상태가 됐을 때 등록해뒀던 콜백이 실행되게 할 수 있음
+-> 콜백 함수
+*/
+
+//promise chaining
+// : 비동기작업을 순차적으로 진행해야 할 때.
+
+//catch 메소드
+
+fetch ('https://jsonplaceholder.typicode.com/users')
+.then ((response)=>response.text())
+.catch((error)=>{console.log(error);})
+.then(undefined,(error)=>{console.log(error);}) //이거랑 위의 줄 catch문이랑 완전히 같은 의미를 갖는다
+//그니까, catch 메소드는 then 메소드를 변형한 것이라고 알아둬도 됨.
+.then((result)=>{console.log(result);})
+.finally(()=>{console.log('exit');})
+//항상 실행해야 하는 코드는 finally 메소드를 사용함
+//보통 catch 메소드 바로 뒤에 쓴다.
+
